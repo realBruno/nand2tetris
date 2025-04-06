f6c4b96f55e2d10680c6d8da461e8ee70fd32378
Bruno <soubruno@protonmail.com>
2025-04-06 20:20:54 -0300

    Add AND gate in JS

---
+++ projects/1/js/AND.mjs
@@ -0,0 +1,18 @@
+/* AND gate */
+
+import { NAND } from "./NAND.mjs";
+import { dotTst } from "./dottst.mjs";
+
+class AND {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    Simulate () {
+        let a = new NAND(this.a, this.b).Simulate();
+        return new NAND(a, a).Simulate();
+    } 
+}
+let and;
+dotTst(and, AND)
